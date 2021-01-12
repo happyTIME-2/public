3cc7f17c381607ede272600f7eed9da18c00d423
johnsqliu <johnsqliu@tencent.com>
2021-01-12 11:39:05 +0800

    update checkSignature

--- handler/checkSignature.js
+++ handler/checkSignature.js
@@ -1,36 +1,12 @@
 const { config } = require('../config')
-const { createHash } = require('crypto');
-
-/**
- * @param {string} algorithm
- * @param {any} content
- *  @return {string}
- */
-const encrypt = (algorithm, content) => {
-  let hash = createHash(algorithm)
-  hash.update(content)
-  return hash.digest('hex')
-}
-
-/**
- * @param {any} content
- *  @return {string}
- */
-const sha1 = (content) => encrypt('sha1', content)
+const CryptoJS = require('crypto-js')
 
 async function check(signature, timestamp, nonce, msg_encrypt='') 
 {
   const { token } = config;
   const list = [token, timestamp, nonce, msg_encrypt];
   list.sort();
-  let tmpStr = '';
-  list.forEach(element => {
-    tmpStr += element;
-  });
-
-  if(msg_encrypt !== '') tmpStr += ',' + msg_encrypt
-
-  const sign = sha1(tmpStr);
+  const sign = CryptoJS.SHA1(list.join("")).toString();
 
   console.log(`sign:${sign}, signature: ${signature}, msg_encrypt: ${msg_encrypt}`)
 
