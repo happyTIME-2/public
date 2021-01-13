aabde01a8bbc6c7c1c5ab7c1c9842a3628cd1fac
johnsqliu <johnsqliu@tencent.com>
2021-01-13 18:45:52 +0800

    update

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,6 +3,7 @@ const { config } = require('../config');
 const { check } = require('../handler/checkSignature');
 const wxBizMsgCrypt = require('../handler/wxBizMsgCrypt');
 const xmlparser = require('express-xml-bodyparser')
+const { parseString } = require('xml2js')
 
 const wxMsgCrypt = new wxBizMsgCrypt();
 
@@ -22,7 +23,7 @@ const verification = async(req, res) => {
   }
 }
 
-router.all('/check', xmlparser({trim: false, explicitArray: false}), async(req, res, next) => {
+router.all('/check', async(req, res, next) => {
   if(req.method == 'POST') {
     let data = '';
     req.on('data', (chunk) => {
@@ -31,32 +32,36 @@ router.all('/check', xmlparser({trim: false, explicitArray: false}), async(req, 
 
     req.on('end', async() => {
       const { signature, timestamp, nonce, openid, encrypt_type, msg_signature } = req.query;
-      const postData = data;
+      parseString(data, { trim: false, explicitArray: false }, (err, result) => {
+        if (err) throw err
 
-      console.log(postData)
+        const postData = result
+
+        console.log(postData)
       
-      try {
-        const msg = await wxMsgCrypt.decryptMsg(msg_signature,timestamp, nonce, postData)
+        try {
+          const msg = await wxMsgCrypt.decryptMsg(msg_signature,timestamp, nonce, postData)
 
-        const content  = 'Hello World!'
-        const replyNonce =  parseInt((Math.random() * 100000000000), 10)
-        const createTime = Date.now()
+          const content  = 'Hello World!'
+          const replyNonce =  parseInt((Math.random() * 100000000000), 10)
+          const createTime = Date.now()
 
-        const { ToUserName, FromUserName, MsgType } = msg
+          const { ToUserName, FromUserName, MsgType } = msg
 
-        const replyMsg = `<xml><ToUserName><![CDATA[${ToUserName}]]></ToUserName><FromUserName><![CDATA[${FromUserName}]]></FromUserName><CreateTime>${createTime}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${content}]]></Content></xml>`
+          const replyMsg = `<xml><ToUserName><![CDATA[${ToUserName}]]></ToUserName><FromUserName><![CDATA[${FromUserName}]]></FromUserName><CreateTime>${createTime}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${content}]]></Content></xml>`
 
-        const result = await wxMsgCrypt.encryptMsg(replyMsg, {
-          timestamp: createTime, nonce: replyNonce
-        })
+          const result = await wxMsgCrypt.encryptMsg(replyMsg, {
+            timestamp: createTime, nonce: replyNonce
+          })
 
-        console.log(`msg: ${msg}`);
-        console.log(`result: ${result}`);
+          console.log(`msg: ${msg}`);
+          console.log(`result: ${result}`);
 
-        res.send(replyMsg)
-      } catch(e) {
-        throw new Error(e)
-      }
+          res.send(replyMsg)
+        } catch(e) {
+          throw new Error(e)
+        }
+      })
     })
   } else {
     await verification(req, res)
