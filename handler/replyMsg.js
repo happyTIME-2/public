41a7655b275d34a9448f7bc524a616e8dfb474a1
johnsqliu <johnsqliu@tencent.com>
2021-01-22 11:50:24 +0800

    添加完善回复消息的各自类型

--- handler/replyMsg.js
+++ handler/replyMsg.js
@@ -21,5 +21,111 @@ const textMsg = (ToUserName, FromUserName, content) => {
   return msg
 }
 
+/**
+  * @param { string } FromUserName (发送者) ,公众号自动回复则为公众号OpenID
+  * @param { string } ToUserName (接收者) ,公众号自动回复则为接收消息的用户的微信号
+  * @param { string } MediaId 通过素材管理中的接口上传多媒体文件，得到的id
+*/
+const imageMsg = (ToUserName, FromUserName, MediaId) => {
+  return `<xml>
+    <ToUserName><![CDATA[${ToUserName}]]></ToUserName>
+    <FromUserName><![CDATA[${FromUserName}]]></FromUserName>
+    <CreateTime>${Date.now()}</CreateTime>
+    <MsgType><![CDATA[image]]></MsgType>
+    <Image>
+      <MediaId><![CDATA[${MediaId}]]></MediaId>
+    </Image>
+  </xml>`;
+}
 
-module.exports = { textMsg };
+/**
+  * @param { string } FromUserName (发送者) ,公众号自动回复则为公众号OpenID
+  * @param { string } ToUserName (接收者) ,公众号自动回复则为接收消息的用户的微信号
+  * @param { string } MediaId 通过素材管理中的接口上传多媒体文件，得到的id
+*/
+const voiceMsg = (ToUserName, FromUserName, MediaId) => {
+  return `<xml>
+    <ToUserName><![CDATA[${ToUserName}]]></ToUserName>
+    <FromUserName><![CDATA[${FromUserName}]]></FromUserName>
+    <CreateTime>${Date.now()}</CreateTime>
+    <MsgType><![CDATA[voice]]></MsgType>
+    <Image>
+      <MediaId><![CDATA[${MediaId}]]></MediaId>
+    </Image>
+  </xml>`;
+}
+
+/**
+  * @param { string } FromUserName (发送者) ,公众号自动回复则为公众号OpenID
+  * @param { string } ToUserName (接收者) ,公众号自动回复则为接收消息的用户的微信号
+  * @param { string } MediaId 通过素材管理中的接口上传多媒体文件，得到的id
+  * @param { string } title 视频消息的标题
+  * @param { string } description 视频消息的描述
+*/
+const videoMsg = (ToUserName, FromUserName, MediaId, title, description) => {
+  return `<xml>
+    <ToUserName><![CDATA[${ToUserName}]]></ToUserName>
+    <FromUserName><![CDATA[${FromUserName}]]></FromUserName>
+    <CreateTime>${Date.now()}</CreateTime>
+    <MsgType><![CDATA[video]]></MsgType>
+    <Video>
+      <MediaId><![CDATA[${MediaId}]]></MediaId>
+      <Title><![CDATA[${title}]]></Title>
+      <Description><![CDATA[${description}]]></Description>
+    </Video>
+  </xml>`;
+}
+
+/**
+  * @param { string } FromUserName (发送者) ,公众号自动回复则为公众号OpenID
+  * @param { string } ToUserName (接收者) ,公众号自动回复则为接收消息的用户的微信号
+  * @param { string } title 音乐标题
+  * @param { string } description 音乐描述
+  * @param { string } musicUrl 音乐链接
+  * @param { string } hQMusicUrl 高质量音乐链接，WIFI环境优先使用该链接播放音乐
+  * @param { string } thumbMediaId 缩略图的媒体id，通过素材管理中的接口上传多媒体文件，得到的id
+*/
+const musicMsg = (ToUserName, FromUserName, title, description, musicUrl, hQMusicUrl, thumbMediaId) => {
+  return `<xml>
+    <ToUserName><![CDATA[${ToUserName}]]></ToUserName>
+    <FromUserName><![CDATA[${FromUserName}]]></FromUserName>
+    <CreateTime>${Date.now()}</CreateTime>
+    <MsgType><![CDATA[music]]></MsgType>
+    <Music>
+      <Title><![CDATA[${title}]]></Title>
+      <Description><![CDATA[${description}]]></Description>
+      <MusicUrl><![CDATA[${musicUrl}]]></MusicUrl>
+      <HQMusicUrl><![CDATA[${hQMusicUrl}]]></HQMusicUrl>
+      <ThumbMediaId><![CDATA[${thumbMediaId}]]></ThumbMediaId>
+    </Music>
+  </xml>`;
+}
+
+/**
+  * @param { string } FromUserName (发送者) ,公众号自动回复则为公众号OpenID
+  * @param { string } ToUserName (接收者) ,公众号自动回复则为接收消息的用户的微信号
+  * @param { int } articleCount 图文消息个数；当用户发送文本、图片、语音、视频、图文、地理位置这六种消息时，开发者只能回复1条图文消息；其余场景最多可回复8条图文消息
+  * @param { string } title 图文消息标题
+  * @param { string } description 图文消息描述
+  * @param { string } picurl 图片链接，支持JPG、PNG格式，较好的效果为大图360*200，小图200*200
+  * @param { string } url 通点击图文消息跳转链接
+*/
+const newsMsg = (ToUserName, FromUserName, articleCount = 1, title, description, picurl, url) => {
+  return `<xml>
+    <ToUserName><![CDATA[${ToUserName}]]></ToUserName>
+    <FromUserName><![CDATA[${FromUserName}]]></FromUserName>
+    <CreateTime>${Date.now()}</CreateTime>
+    <MsgType><![CDATA[news]]></MsgType>
+    <ArticleCount>${articleCount}</ArticleCount>
+    <Articles>
+      <item>
+        <Title><![CDATA[${title}]]></Title>
+        <Description><![CDATA[${description}]]></Description>
+        <PicUrl><![CDATA[${picurl}]]></PicUrl>
+        <Url><![CDATA[${url}]]></Url>
+      </item>
+    </Articles>
+  </xml>`;
+}
+
+module.exports = { textMsg, voiceMsg, videoMsg, musicMsg, newsMsg };
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { check } = require('../handler/checkSignature');
 const wxBizMsgCrypt = require('../handler/wxBizMsgCrypt');
 const xmlparser = require('express-xml-bodyparser')
-const { textMsg } = require('../handler/replyMsg')
+const { textMsg, voiceMsg, videoMsg, musicMsg, newsMsg } = require('../handler/replyMsg')
 
 const router = express.Router();
 
@@ -31,7 +31,7 @@ router.all('/check', xmlparser({trim: false, explicitArray: false}), async(req, 
 
       // 回复消息跟接收到的消息体内的ToUserName跟FromUserName要对调
       const { ToUserName, FromUserName, MsgType } = msg
-      const replyMsg = textMsg(FromUserName, ToUserName, '你好，欢迎调戏前端探索者公众号！')
+      const replyMsg = newsMsg(FromUserName, ToUserName, 1, '图片合成类H5总结', '最近越来越多图片合成类的H5需求，让用户通过H5页面的交互选择，最后根据用户的选择生成一张合成图片，让用户长按保存图片并引导用户分享该图片', 'https://www.imiaomeng.com/uploads/allimg/190712/1-1ZG209342bO.jpg', 'https://mp.weixin.qq.com/s/6s2p3976CUH6XxxF4pdmkg')
       const result = await wxMsgCrypt.encryptMsg(replyMsg, {
         timestamp: createTime, nonce: replyNonce
       })
