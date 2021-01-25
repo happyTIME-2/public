c1228b7f97247b5f02238cb5fdba4c092fafdca4
johnsqliu <johnsqliu@tencent.com>
2021-01-25 15:57:22 +0800

    replyMsg

--- handler/history.js
+++ handler/history.js
@@ -5,8 +5,8 @@ const history = async() => {
   const list = await api.getMediaList('news', 0 ,6)
 
   let links = '';
-  list.map(i => {
-    links += `<a href='${i.url}'>${i.title}</a>`
+  list.map((i,index) => {
+    links += `${index+1}.<a href='${i.url}'>${i.title}</a><br />`
   })
 
   return links;
