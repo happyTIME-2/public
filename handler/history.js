306e2585cce2a77b6e87bec9a29b14c47ff208c5
johnsqliu <johnsqliu@tencent.com>
2021-01-25 16:06:38 +0800

    history

--- handler/history.js
+++ handler/history.js
@@ -4,7 +4,7 @@ const history = async() => {
   const api = new wechatApi()
   const list = await api.getMediaList('news', 0 ,6)
 
-  let links = '<h2>历史文章列表:</h2> \n';
+  let links = '历史文章列表: \n';
   list.map((i,index) => {
     links += `${index+1}.<a href='${i.url}'>${i.title}</a> \n`
   })
