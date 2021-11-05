7cb150c3966b457e76688b5dc53829d0e541108d
johnsqliu <johnsqliu@tencent.com>
2021-11-05 16:46:50 +0800

    (feat) ssq 公众号自动回复添加双色球数据查询

---
+++ handler/ssq/index.js
@@ -0,0 +1,49 @@
+const { config } = require('../../config')
+const axios = require('axios').default
+
+const auth = () => {
+  const nowDate = new Date(); 
+  const dateTime = nowDate.toUTCString();
+  const SecretId = config.SCF_Secret_Id;
+  const SecretKey = config.SCF_Secret_Key;
+  const source = 'xxxxxx'; 
+  const auth = "hmac id=\"" + SecretId + "\", algorithm=\"hmac-sha1\", headers=\"x-date source\", signature=\"";
+  const signStr = "x-date: " + dateTime + "\n" + "source: " + source;
+
+  let sign = CryptoJS.HmacSHA1(signStr, SecretKey)
+  sign = CryptoJS.enc.Base64.stringify(sign)
+  sign = auth + sign + "\""
+  
+  return {
+    source,
+    dateTime,
+    sign
+  }
+}
+
+const getSsqData = async () => {
+  const apiUrl = "https://service-n8z6vr4y-1255917349.gz.apigw.tencentcs.com/release/ssq";
+  const { source, dateTime, sign } = auth();
+
+  const instance = axios.create({
+    baseURL: apiUrl,
+    timeout: 5000,
+    headers: {
+      "Source":source,
+      "X-Date":dateTime,
+      "Authorization":sign
+    },
+    withCredentials: true
+  });
+  
+  return new Promise((resolve, reject) => {
+    instance.get().then(res => {
+      resolve(res.data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+};
+
+module.exports = getSsqData;
+
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,6 +4,7 @@ const wxBizMsgCrypt = require('../handler/wxBizMsgCrypt');
 const xmlparser = require('express-xml-bodyparser')
 const { textMsg, voiceMsg, videoMsg, musicMsg, newsMsg } = require('../handler/replyMsg')
 const history = require('../handler/history')
+const getSsqData = require('../handler/ssq/index')
 
 const router = express.Router();
 
@@ -31,13 +32,27 @@ router.all('/check', xmlparser({trim: false, explicitArray: false}), async(req, 
       const createTime = Date.now()
 
       // 回复消息跟接收到的消息体内的ToUserName跟FromUserName要对调
-      const { ToUserName, FromUserName, MsgType, Content } = msg
+      const { ToUserName, FromUserName, MsgType } = msg
 
-      let replyMsg = newsMsg(FromUserName, ToUserName, 1, '图片合成类H5总结', '最近越来越多图片合成类的H5需求，让用户通过H5页面的交互选择，最后根据用户的选择生成一张合成图片，让用户长按保存图片并引导用户分享该图片', 'https://www.imiaomeng.com/uploads/allimg/190712/1-1ZG209342bO.jpg', 'https://mp.weixin.qq.com/s/6s2p3976CUH6XxxF4pdmkg')
-      
-      if(Content.includes('历史')) {
-        const links = await history()
-        replyMsg = textMsg(FromUserName, ToUserName, links)
+      if(MsgType === 'text') {
+        const content = msg.Content;
+        let replyMsg = newsMsg(FromUserName, ToUserName, 1, '图片合成类H5总结', '最近越来越多图片合成类的H5需求，让用户通过H5页面的交互选择，最后根据用户的选择生成一张合成图片，让用户长按保存图片并引导用户分享该图片', 'https://www.imiaomeng.com/uploads/allimg/190712/1-1ZG209342bO.jpg', 'https://mp.weixin.qq.com/s/6s2p3976CUH6XxxF4pdmkg')
+        
+        if(content.includes('历史')) {
+          const links = await history()
+          replyMsg = textMsg(FromUserName, ToUserName, links)
+        }
+
+        if(content.includes('双色球') || content.includes('福彩') || content.includes('ssq')) {
+          const data = await getSsqData()
+          replyMsg = textMsg(FromUserName, ToUserName, data)
+        }
+      }
+
+      if(MsgType === 'voice') {
+        const MediaId = msg.MediaId
+
+        
       }
 
       const result = await wxMsgCrypt.encryptMsg(replyMsg, {
