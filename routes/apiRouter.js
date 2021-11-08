0592ad3d7a8fceea014b31b8194177794467667b
johnsqliu <johnsqliu@tencent.com>
2021-11-08 11:29:49 +0800

    reset

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -32,39 +32,16 @@ router.all('/check', xmlparser({trim: false, explicitArray: false}), async(req, 
       const createTime = Date.now()
 
       // 回复消息跟接收到的消息体内的ToUserName跟FromUserName要对调
-      const { ToUserName, FromUserName, MsgType } = msg
-      console.log('msg:', msg);
-      if(MsgType === 'text') {
-        const content = msg.Content;
-        console.log('content:', content);
-        let replyMsg = newsMsg(FromUserName, ToUserName, 1, '图片合成类H5总结', '最近越来越多图片合成类的H5需求，让用户通过H5页面的交互选择，最后根据用户的选择生成一张合成图片，让用户长按保存图片并引导用户分享该图片', 'https://www.imiaomeng.com/uploads/allimg/190712/1-1ZG209342bO.jpg', 'https://mp.weixin.qq.com/s/6s2p3976CUH6XxxF4pdmkg')
-        
-        console.log('replyMsg:', replyMsg);
-        if(content.includes('历史')) {
-          const links = await history()
-          replyMsg = textMsg(FromUserName, ToUserName, links)
-        }
-
-        // if(content.includes('双色球') || content.includes('福彩') || content.includes('ssq')) {
-        //   const data = await getSsqData()
-        //   console.log(data);
-        //   replyMsg = textMsg(FromUserName, ToUserName, data)
-        // }
+      const { ToUserName, FromUserName, MsgType, Content } = msg
+      let replyMsg = newsMsg(FromUserName, ToUserName, 1, '图片合成类H5总结', '最近越来越多图片合成类的H5需求，让用户通过H5页面的交互选择，最后根据用户的选择生成一张合成图片，让用户长按保存图片并引导用户分享该图片', 'https://www.imiaomeng.com/uploads/allimg/190712/1-1ZG209342bO.jpg', 'https://mp.weixin.qq.com/s/6s2p3976CUH6XxxF4pdmkg')
+      
+      if(Content.includes('历史')) {
+        const links = await history()
+        replyMsg = textMsg(FromUserName, ToUserName, links)
       }
-
-      // if(MsgType === 'voice') {
-      //   const MediaId = msg.MediaId
-
-        
-      // }
-
-      console.log('replyMsg2:', replyMsg);
       const result = await wxMsgCrypt.encryptMsg(replyMsg, {
         timestamp: createTime, nonce: replyNonce
       })
-
-      console.log('result:', result);
-
       res.send(result)
     } catch(e) {
       throw new Error(e)
